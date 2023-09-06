81f04da26fc862c8996031baa25b228f1e4117b1
ayushrajdahal <ayushrajdahal@gmail.com>
2023-09-06 01:59:49 -0500

    Closed scanner object, better variable name

--- hw1/src/Greeting.java
+++ hw1/src/Greeting.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Greeting {
 	public static void main(String[] args) {
-		Scanner myObject = new Scanner(System.in); // creates a scanner object
+		Scanner scanner = new Scanner(System.in); // creates a scanner object
 
 		System.out.println("What is your name?"); // input message for name
-		String firstName = myObject.nextLine(); // takes a name input
+		String firstName = scanner.nextLine(); // takes a name input
 
 		System.out.println("What salutation would you like me to use?"); // input message for salutation
-		String salutation = myObject.nextLine(); // takes a salutation input
+		String salutation = scanner.nextLine(); // takes a salutation input
 
 		System.out.println("Hello " + salutation + " " + firstName + ", it is nice to meet you! Goodbye!"); // prints out the greeting
+		scanner.close(); // closes the scanner
 	}
 }
