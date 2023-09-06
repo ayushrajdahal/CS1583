e835c298dd00ab4ad59e54232a31b2cb18dc31f3
ayushrajdahal <ayushrajdahal@gmail.com>
2023-09-06 12:19:13 -0500

    Reverted HW1 to initial commit for merge - attempt 2

--- hw1/src/Greeting.java
+++ hw1/src/Greeting.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Greeting {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in); // creates a scanner object
+		Scanner myObject = new Scanner(System.in); // creates a scanner object
 
 		System.out.println("What is your name?"); // input message for name
-		String firstName = scanner.nextLine(); // takes a name input
+		String firstName = myObject.nextLine(); // takes a name input
 
 		System.out.println("What salutation would you like me to use?"); // input message for salutation
-		String salutation = scanner.nextLine(); // takes a salutation input
+		String salutation = myObject.nextLine(); // takes a salutation input
 
 		System.out.println("Hello " + salutation + " " + firstName + ", it is nice to meet you! Goodbye!"); // prints out the greeting
-		scanner.close(); // closes the scanner
 	}
 }
