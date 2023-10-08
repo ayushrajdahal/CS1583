6ce8c5408228be3843a262754b6957b517b07753
ayushrajdahal <ayushrajdahal@gmail.com>
2023-10-08 18:05:08 -0500

    HW5: Added README text, code comments

---
+++ hw5/README.md
@@ -0,0 +1,32 @@
+# Homework 5 - Random Guesser
+
+## How to run
+
+```
+$ javac RandomGuesser.java
+$ java RandomGuesser
+```
+
+## Example Outputs
+
+```
+Max Boundary:10
+Min Boundary:8
+Max No. of Guesses:1
+Guess a number:9
+CORRECT
+Your grade is LUCKY
+```
+
+```
+Max Boundary:1
+Min Boundary:10
+Max No. of Guesses:4
+Guess a number:2
+HIGHER
+Guess a number:6
+HIGHER
+Guess a number:9
+CORRECT
+Your grade is EXCELLENT
+```
--- hw5/src/RandomGuesser.java
+++ hw5/src/RandomGuesser.java
@@ -1,30 +1,23 @@
-import java.lang.Math;
 import java.util.*;
 
 public class RandomGuesser {
 
-	// return the log base 2 of ain integer as a double
-	private static double log2Func(int inputNum) {
-
+	// Calculate log base 2 of inputNum
+	private static double log2(int inputNum) {
 		return Math.log(inputNum) / Math.log(2);
-
 	}
 
-	// randomly picks a number between two integers (non-inclusive)
-	private static int randomPicker(int a, int b) {
-
+	// Generate a random number between min_boundary and max_boundary
+	private static int randomPicker(int min_boundary, int max_boundary) {
 		Random rand = new Random();
-		a++;
-		return rand.nextInt(b - a) + a;
-
+		return rand.nextInt(max_boundary - min_boundary - 1) + min_boundary + 1;
 	}
 
-	// gives EXCELLENT, GOOD, FAILED, and LUCKY acc to the requirement.
+	// Determine the grade of the guesser based on the number of tries and whether
+	// or not they guessed correctly
 	private static void guessGrading(int max_boundary, int min_boundary, int number_tries, boolean guessed) {
-		double log2value = log2Func(max_boundary - min_boundary);
-
+		double log2value = log2(max_boundary - min_boundary);
 		String message;
-
 		if (!guessed) {
 			message = "FAILED";
 		} else if ((max_boundary - min_boundary > 1) && (number_tries == 1)) {
@@ -34,70 +27,73 @@ public class RandomGuesser {
 		} else {
 			message = "GOOD";
 		}
-
 		System.out.println("Your grade is " + message);
 		System.exit(0);
-
 	}
 
-	// main method
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Max Boundary:");
+		System.out.print("Max Boundary:");
 		int max_boundary = scanner.nextInt();
-		System.out.println("Min Boundary:");
+		System.out.print("Min Boundary:");
 		int min_boundary = scanner.nextInt();
-		System.out.println("Max No. of Guesses:");
+		System.out.print("Max No. of Guesses:");
 		int max_guesses = scanner.nextInt();
 
-		int temp;
 		List<Integer> prev_guesses = new ArrayList<>();
 
-		// to make sure max is max and min is min
+		// Check if the boundaries are valid
+		int temp;
+		temp = max_boundary;
+		max_boundary = Math.max(min_boundary, max_boundary);
+		min_boundary = Math.min(min_boundary, temp);
 
-		if (max_boundary < min_boundary) {
-			temp = max_boundary;
-			max_boundary = min_boundary;
-			min_boundary = temp;
-		}
-
-		// throw an error if there are <=1 integers in between
-
+		// Check if the range is valid
 		if (max_boundary - min_boundary <= 1) {
 			System.out.println("ERROR: The two integers should have at least one integer in between");
+			System.out.println(max_boundary + " - " + min_boundary + " = " + (max_boundary - min_boundary));
 			System.exit(0);
 		}
 
-		// throw an error if inputted max no. of guesses < 1
+		// Check if the number of guesses is valid
 		if (max_guesses < 1) {
 			System.out.println("ERROR: Max no. of guesses shouldn't be less than 1");
 			System.exit(0);
 		}
 
+		// Generate a random number within the range
 		int generated_number = randomPicker(min_boundary, max_boundary);
 
+		// Loop through the guesses
 		for (int i = 0; i < max_guesses; i++) {
-			System.out.println("Guess a number:");
+			System.out.print("Guess a number:");
 			int anumber = scanner.nextInt();
 
+			// Check for duplicate guesses
 			if (prev_guesses.contains(anumber)) {
 				System.out.println("DUPLICATE GUESS");
 				i--;
 			} else if (anumber == generated_number) {
+				// The user guessed the number correctly
 				System.out.println("CORRECT");
 				guessGrading(max_boundary, min_boundary, i + 1, true);
 			} else if (anumber < generated_number) {
+				// The user guessed too low
 				System.out.println("HIGHER");
 				prev_guesses.add(anumber);
 			} else {
+				// The user guessed too high
 				System.out.println("LOWER");
 				prev_guesses.add(anumber);
 			}
 		}
+
+		// The user failed to guess the number
 		System.out.println("Sorry, the right answer was " + generated_number + ".");
 		guessGrading(max_boundary, min_boundary, max_guesses, false);
+
 		scanner.close();
 	}
 }
