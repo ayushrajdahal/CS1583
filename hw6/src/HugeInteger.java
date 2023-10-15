4304e35703944b9cd5d4e99705eadc5f7372f100
ayushrajdahal <ayushrajdahal@gmail.com>
2023-10-14 19:26:17 -0500

    HW6: bonus section, better README, proper tests

--- hw6/src/HugeInteger.java
+++ hw6/src/HugeInteger.java
@@ -1,56 +1,79 @@
 public class HugeInteger {
+    // makes digitsArray variable accessible within the class
     private int[] digitsArray;
 
     public HugeInteger(String num) {
+        // creates an array of 60 integers to store digits
         digitsArray = new int[60];
+
+        // get numeric value of each character of the string
         for (int i = 0; i < num.length(); i++) {
             digitsArray[60 - num.length() + i] = Character.getNumericValue(num.charAt(i));
         }
     }
 
     public static HugeInteger parse(String num) {
+        // uses regular expressions to remove everything except digits in num
         String numericString = num.replaceAll("[^0-9]", "");
         return new HugeInteger(numericString);
     }
 
+    @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        boolean leadingZero = true;
+        // initialize the string
+        String sb = "";
+        // deals with leading zero
+        boolean beforeNumber = true;
+
         for (int i = 0; i < digitsArray.length; i++) {
+            // avoids zeros being added to the string before the actual number starts
             if (digitsArray[i] != 0) {
-                leadingZero = false;
+                beforeNumber = false;
             }
-            if (!leadingZero) {
-                sb.append(digitsArray[i]);
+            if (!beforeNumber) {
+                sb += String.valueOf(digitsArray[i]);
             }
         }
-        if (sb.length() == 0) {
-            sb.append(0);
+        // deals with empty string, changing it to zero
+        if (sb == "") {
+            sb += "0";
         }
-        return sb.toString();
+        return sb;
     }
 
     public void add(int number) {
+        // calls the same module passing number as HugeString
         add(new HugeInteger(String.valueOf(number)));
     }
 
     public void add(HugeInteger number) {
+        // deals with carry overs
         int carry = 0;
+
+        // loops digitsArray.length times
         for (int i = digitsArray.length - 1; i >= 0; i--) {
+            // adds previous carry to the individual digit sum
             int sum = digitsArray[i] + number.digitsArray[i] + carry;
-            digitsArray[i] = sum % 10;
+            // update carry with new value
             carry = sum / 10;
+            // removes preceeding digit since it is used in carry
+            digitsArray[i] = sum % 10;
         }
     }
 
     public void subtract(int number) {
+        // calls the same module passing number as HugeString
         subtract(new HugeInteger(String.valueOf(number)));
     }
 
     public void subtract(HugeInteger number) {
+        // stores borrowed values
         int borrow = 0;
+        // loops digitsArray.length times
         for (int i = digitsArray.length - 1; i >= 0; i--) {
+            // subtracts previous borrow from the individual digit difference
             int diff = digitsArray[i] - number.digitsArray[i] - borrow;
+            // borrows 1 if diff becomes less than zero, 0 if more.
             if (diff < 0) {
                 diff += 10;
                 borrow = 1;
@@ -62,10 +85,13 @@ public class HugeInteger {
     }
 
     public boolean isEqualTo(int number) {
+        // calls the same module passing number as HugeString
         return isEqualTo(new HugeInteger(String.valueOf(number)));
     }
 
     public boolean isEqualTo(HugeInteger number) {
+
+        // loops over each digit in the digitArray, checks if all of them are equal.
         for (int i = 0; i < digitsArray.length; i++) {
             if (digitsArray[i] != number.digitsArray[i]) {
                 return false;
@@ -75,10 +101,13 @@ public class HugeInteger {
     }
 
     public boolean isGreaterThan(int number) {
+        // calls the same module passing number as HugeString
         return isGreaterThan(new HugeInteger(String.valueOf(number)));
     }
 
     public boolean isGreaterThan(HugeInteger number) {
+        // starts with the most significant digits, and goes on.
+        // if any of the digit is bigger/smaller, we have a greater/smaller value.
         for (int i = 0; i < digitsArray.length; i++) {
             if (digitsArray[i] > number.digitsArray[i]) {
                 return true;
@@ -90,22 +119,27 @@ public class HugeInteger {
     }
 
     public boolean isGreaterThanOrEqualTo(int number) {
+        // calls the same module passing number as HugeString
         return isGreaterThanOrEqualTo(new HugeInteger(String.valueOf(number)));
     }
 
     public boolean isGreaterThanOrEqualTo(HugeInteger number) {
+        // uses pre-existing modules
         return isEqualTo(number) || isGreaterThan(number);
     }
 
     public boolean isLessThan(int number) {
+        // calls the same module passing number as HugeString
         return isLessThan(new HugeInteger(String.valueOf(number)));
     }
 
     public boolean isLessThan(HugeInteger number) {
+        // negates the output of isGreaterThanOrEqualTo()
         return !isGreaterThanOrEqualTo(number);
     }
 
     public boolean isLessThanOrEqualTo(int number) {
+        // calls the same module passing number as HugeString
         return isLessThanOrEqualTo(new HugeInteger(String.valueOf(number)));
     }
 
@@ -114,12 +148,98 @@ public class HugeInteger {
     }
 
     public boolean isZero() {
+
+        // returns false if any of the digit is anything other than zero.
         for (int i = 0; i < digitsArray.length; i++) {
             if (digitsArray[i] != 0) {
                 return false;
             }
         }
+
+        // if nothing is returned in the for loop above, it is a zero
         return true;
     }
 
+    public void multiply(int number) {
+        // calls the same module passing number as HugeString
+        multiply(new HugeInteger(String.valueOf(number)));
+    }
+
+    // multiplies the current HugeInteger object with the passed HugeInteger object
+    public void multiply(HugeInteger number) {
+        // initializes an array to store the result of multiplication
+        int[] result = new int[120];
+
+        // multiplies the digits of the current HugeInteger object with the digits of
+        // the passed HugeInteger object
+        for (int i = digitsArray.length - 1; i >= 0; i--) {
+            int carry = 0;
+            for (int j = number.digitsArray.length - 1; j >= 0; j--) {
+                int product = digitsArray[i] * number.digitsArray[j] + carry + result[i + j + 1];
+                result[i + j + 1] = product % 10;
+                carry = product / 10;
+            }
+            result[i] += carry;
+        }
+
+        // initializes a new array to store the digits of the result
+        digitsArray = new int[60];
+
+        // copies the digits from the result array to the digitsArray of the current
+        // HugeInteger object
+        for (int i = 0; i < digitsArray.length; i++) {
+            digitsArray[i] = result[i + 60];
+        }
+    }
+
+    public void divide(int number) {
+        // calls the same module passing number as HugeString
+        divide(new HugeInteger(String.valueOf(number)));
+    }
+
+    public void divide(HugeInteger number) {
+        // initializing quotient and remiander
+        HugeInteger quotient = new HugeInteger("0");
+        HugeInteger remainder = new HugeInteger("0");
+
+        for (int i = 0; i < digitsArray.length; i++) {
+
+            // equivalent to remainder * 10 + digitsArray[i]
+            remainder.multiply(10);
+            remainder.add(digitsArray[i]);
+
+            // used for quotient
+            int q = 0;
+
+            while (remainder.isGreaterThanOrEqualTo(number)) {
+                remainder.subtract(number);
+                q++;
+            }
+            quotient.multiply(10);
+            quotient.add(q);
+        }
+        digitsArray = quotient.digitsArray;
+    }
+
+    public void remainder(int number) {
+        // calls the same module passing number as HugeString
+        remainder(new HugeInteger(String.valueOf(number)));
+    }
+
+    public void remainder(HugeInteger number) {
+        HugeInteger remainder = new HugeInteger("0");
+        HugeInteger quotient = new HugeInteger("0");
+        for (int i = 0; i < digitsArray.length; i++) {
+            remainder.multiply(10);
+            remainder.add(digitsArray[i]);
+            int q = 0;
+            while (remainder.isGreaterThanOrEqualTo(number)) {
+                remainder.subtract(number);
+                q++;
+            }
+            quotient.multiply(10);
+            quotient.add(q);
+        }
+        digitsArray = remainder.digitsArray;
+    }
 }
--- hw6/src/HugeIntegerTest.java
+++ hw6/src/HugeIntegerTest.java
@@ -1,25 +1,69 @@
 public class HugeIntegerTest {
     public static void main(String[] args) {
+
         HugeInteger A = new HugeInteger("12345678901234567890123456789");
-        HugeInteger B = HugeInteger.parse("987654321098765432109876543");
+        String A_temp = A.toString(); // a copy of variable A as a String
+
+        HugeInteger B = HugeInteger.parse("abc987654321098765432109876543");
+        int C = 123;
 
         System.out.println("\nInitializing HugeInteger A and B:\n");
-        System.out.printf("A = %s\n", A);
-        System.out.printf("B = %s\n", B);
+        System.out.printf("HugeInteger A = %s\n", A);
+        System.out.printf("HugeInteger B = %s\n", B);
+        System.out.printf("int C = %s\n", C);
 
         System.out.println("\nTesting methods:\n");
 
+        System.out.printf("A.toString() = %s\n", A.toString());
         A.add(B);
         System.out.printf("A + B = %s\n", A);
+        A = new HugeInteger(A_temp);
+        A.add(C);
+        System.out.printf("A + C = %s\n", A);
+        A = new HugeInteger(A_temp);
 
         A.subtract(B);
         System.out.printf("A - B = %s\n", A);
+        A = new HugeInteger(A_temp);
+        A.subtract(C);
+        System.out.printf("A - C = %s\n", A);
+        A = new HugeInteger(A_temp);
 
         System.out.printf("A == B: %b\n", A.isEqualTo(B));
+        System.out.printf("A == C: %b\n", A.isEqualTo(C));
+
         System.out.printf("A > B: %b\n", A.isGreaterThan(B));
+        System.out.printf("A > C: %b\n", A.isGreaterThan(C));
+
         System.out.printf("A >= B: %b\n", A.isGreaterThanOrEqualTo(B));
+        System.out.printf("A >= C: %b\n", A.isGreaterThanOrEqualTo(C));
+
         System.out.printf("A < B: %b\n", A.isLessThan(B));
+        System.out.printf("A < C: %b\n", A.isLessThan(C));
+
         System.out.printf("A <= B: %b\n", A.isLessThanOrEqualTo(B));
+        System.out.printf("A <= C: %b\n", A.isLessThanOrEqualTo(C));
+
+        A.multiply(B);
+        System.out.printf("A * B = %s\n", A);
+        A = new HugeInteger(A_temp);
+        A.multiply(C);
+        System.out.printf("A * C = %s\n", A);
+        A = new HugeInteger(A_temp);
+
+        A.divide(B);
+        System.out.printf("A / B = %s\n", A);
+        A = new HugeInteger(A_temp);
+        A.divide(C);
+        System.out.printf("A / C = %s\n", A);
+        A = new HugeInteger(A_temp);
+
+        A.remainder(B);
+        System.out.printf("A %% B = %s\n", A);
+        A = new HugeInteger(A_temp);
+        A.remainder(C);
+        System.out.printf("A %% C = %s\n", A);
+        A = new HugeInteger(A_temp);
 
     }
 }
